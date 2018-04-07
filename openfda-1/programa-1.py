b4b9c95219d3a301bf3f15bc58c3deb27860ff64
Marinacf <marinacastrillof@gmail.com>
2018-04-07 20:49:05 +0200

    Retoques finales

--- openfda-1/programa-1.py
+++ openfda-1/programa-1.py
@@ -1,24 +1,24 @@
-import http.client  # importamos aquellos modulos que vayamos a utilizar posteriormente.
+import http.client  # importamos aquellos módulos que vayamos a utilizar posteriormente.
 import json
 
 headers = {'User-Agent': 'http-client'}
 try:
-    conexion = http.client.HTTPSConnection("api.fda.gov")  # nos conectamos con la pagina pedida por el ej.
+    conexion = http.client.HTTPSConnection("api.fda.gov")  # nos conectamos con la página pedida por el ej.
     conexion.request("GET", "/drug/label.json", None, headers)  # Le mandamos un mensaje de solicitud (verbo:GET).
 except Exception: # utilizamos un try-except, para conectarnos siempre que no haya un error producido por el nombre del servidor.
     print("Error al solicitar el nombre del servidor")
-    exit(1)  # Si hay un error nos salimos del programa
-r1 = conexion.getresponse()  # Leer el mensaje que se recibe
-if r1.status == 404: # En este caso comprobamos si hay un error en el recurso, mediante el estado, sabiendo que si es 200 (OK) y 404 (ERROR)
+    exit(1)  # Si hay un error nos salimos del programa.
+r1 = conexion.getresponse()  # Leer el mensaje que se recibe, una vez establecida la conexión.
+if r1.status == 404: # Comprobamos si hay un error en el recurso, mediante el estado, sabiendo que si es 200 (OK) y 404 (ERROR)
     print("ERROR, recurso no encontrado.")
     exit(1)
-print(r1.status, r1.reason)  # Imprimimos la lina de estado de la respuesta (status:200, reason:OK)
-info_raw = r1.read().decode("utf-8")  # Convertimos el codigo en una cadena
+print(r1.status, r1.reason)  # Imprimimos la linea de estado de la respuesta (status:200, reason:OK)
+info_raw = r1.read().decode("utf-8")  # Convertimos el código en una cadena.
 conexion.close()
-info = json.loads(info_raw)  # transformamos la informacion en cadena, en diccionario, para un mejor uso.
+info = json.loads(info_raw)  # transformamos la información en cadena, en diccionario (info), para un mejor uso.
 
 print("-> El id es", info['results'][0]['id'])
 print("-> El proposito del producto es", info['results'][0]['purpose'][0])
 print("-> El nombre del fabricante es", info['results'][0]['openfda']['manufacturer_name'][0])
-# Para obtener informacion, nos metemos en el diccionario info, en la clave 'results', dentro de ella, en
-# la subclave '0', y una vez ahi buscamos la informacion segun nos pida el ejercicio.
+# Para obtener información, nos metemos en el diccionario info, en la clave 'results', dentro de ella, en
+# la subclave '0', y una vez ahí, buscamos la información según nos pida el ejercicio.
